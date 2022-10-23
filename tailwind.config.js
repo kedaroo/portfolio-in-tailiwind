c127d9a97e50e4e1fac05de731794dfffcbbeb1e
Kedar Basutkar <41827102+kedaroo@users.noreply.github.com>
2022-10-23 13:19:51 +0530

    style: color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'hero-red': '#ed5b42',
+        'hero-orange': '#f4aa85',
+        'hero-blue': '#c3dae0',
+        'hero-gray': '#414247',
+      },
+    },
   },
   plugins: [],
 };
