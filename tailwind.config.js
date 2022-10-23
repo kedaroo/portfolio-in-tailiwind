b980293de1b29de5ab96155a6389d1da503aff93
Kedar Basutkar <41827102+kedaroo@users.noreply.github.com>
2022-10-23 14:35:29 +0530

    style: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'hero-red': '#ed5b42',
-        'hero-orange': '#f4aa85',
-        'hero-blue': '#c3dae0',
-        'hero-gray': '#414247',
+        "hero-red": "#ed5b42",
+        "hero-orange": "#f4aa85",
+        "hero-blue": "#c3dae0",
+        "hero-gray": "#414247",
+        "twitter-blue": "#1DA1F2",
+        "github-gray": "#333",
+        "linkedin-blue": "#0077b5",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-10deg)" },
+          "50%": { transform: "rotate(10deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
       },
     },
   },
